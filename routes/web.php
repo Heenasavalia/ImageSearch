745e89b4a65237a0eed79c232d7df5141c776f55
Heena9dottech <heena.9dot.tech@gmail.com>
2025-08-05 10:01:52 +0530

    set web route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
 
 Route::get('/', function () {
+    return redirect()->route('images.upload.form');
     return view('welcome');
 });
 
